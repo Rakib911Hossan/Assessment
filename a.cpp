00be462ec8aa95dbc3009caae30367546f02d820
shaniul shanto <shantoshaniul@gmail.com>
2024-06-25 17:02:20 +0600

    shanto to shanto prod

--- a.cpp
+++ a.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 using namespace std;
 
+
+// shantos intervene
 int main() {
 
   int first_number, second_number, sum;
 
   cout << "Enter two integers: ";
-  cin >> first_number >> second_number;
-
-  // sum of two numbers in stored in variable sumOfTwoNumbers
-  sum = first_number + second_number;
+  cin >> first_number >> second_number;  
+// product of two numbers in stored in variable sumOfTwoNumbers
+  sum = first_number *  second_number;
 
   // prints sum
-  cout << first_number << " + " <<  second_number << " = " << sum;
+  cout << first_number << " x " <<  second_number << " = " << sum;
 
   return 0;
 }
